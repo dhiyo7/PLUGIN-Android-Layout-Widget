466eba38bb3ea5c556e952779f5ce9abde5a9b46
dhiyo7 <dhiyoreksa12@gmail.com>
2019-11-14 18:54:03 +0700

    tak tambahi kie

--- app/src/main/java/com/plugin/id/Linear.java
+++ app/src/main/java/com/plugin/id/Linear.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class image extends AppCompatActivity {
+public class Linear extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image);
+        setContentView(R.layout.activity_linear);
     }
 }
